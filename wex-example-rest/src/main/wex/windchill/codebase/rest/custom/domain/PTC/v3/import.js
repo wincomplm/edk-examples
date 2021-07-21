b0067b9163fa1069f184356feb8c8210b56967fb
wincomplm <82089131+wincomplm@users.noreply.github.com>
2021-07-21 12:13:58 +0200

    Hello World

---
+++ wex-example-rest/src/main/wex/windchill/codebase/rest/custom/domain/PTC/v3/import.js
@@ -0,0 +1,7 @@
+/**
+ * Function to get a list of all the valid life-cycle states with display and internal value
+ * pairs.
+ */
+function function_HelloWorld(data, params) {
+    return "HelloWorld";
+}
