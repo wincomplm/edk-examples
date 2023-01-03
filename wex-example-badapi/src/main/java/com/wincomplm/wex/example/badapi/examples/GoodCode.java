cec0be48b5e1aaa2585f38ec8dc3e3b4bdc27383
simonh-wincomplm <simonh@wincom-plm.com>
2023-01-03 11:12:48 +0100

    Added bad example

---
+++ wex-example-badapi/src/main/java/com/wincomplm/wex/example/badapi/examples/GoodCode.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2022 Wincom Consulting S.L.
+ * All Rights Reserved.
+ * This source is subject to the terms of a software license agreement.
+ * You shall not disclose such confidential information and shall use it only in accordance with the terms and conditions of the license agreement.
+ */
+
+package com.wincomplm.wex.example.badapi.examples;
+
+/**
+ *
+ * @author SimonHeath
+ */
+public class GoodCode {
+
+}
---
+++ wex-example-badapi/src/main/java/com/wincomplm/wex/example/badapi/impl/bootstrap/WexBootstrap.java
@@ -0,0 +1,23 @@
+
+package com.wincomplm.wex.example.badapi.impl.bootstrap;
+
+import com.wincomplm.wex.kernel.impl.bootstrap.IWexBootstrap;
+import com.wincomplm.wex.kernel.impl.manager.IWexKernelPackage;
+import com.wincomplm.wex.system.impl.scan.WexScanner;
+
+/**
+ *
+ * @author Nassim Bouayad-Agha
+ */
+public class WexBootstrap implements IWexBootstrap {
+
+    @Override
+    public void initialize(IWexKernelPackage wex) throws Exception {
+        WexScanner.scan(wex, "com.wincomplm.wex.example.badapi");
+    }
+
+    @Override
+    public void terminate(IWexKernelPackage wex) throws Exception {
+    }
+
+}
