11727ae5ad228b7213b7e96b8287eb35694c4222
simonh-wincomplm <simonh@wincom-plm.com>
2023-01-09 10:14:08 +0100

    Added network access test

---
+++ wex-example-badapi/src/main/java/com/wincomplm/wex/example/badapi/examples/NetworkAccess.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2022 Wincom Consulting S.L.
+ * All Rights Reserved.
+ * This source is subject to the terms of a software license agreement.
+ * You shall not disclose such confidential information and shall use it only in accordance with the terms and conditions of the license agreement.
+ */
+
+package com.wincomplm.wex.example.badapi.examples;
+ 
+/**
+ *
+ * @author SimonHeath
+ */
+import java.net.URL;
+
+public class NetworkAccess {
+
+    public static void accessNetwork() throws Exception {
+        URL thisURL = new URL(null);
+    }
+}
