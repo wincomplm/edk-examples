505cbda00dcf3013794defbb50911968fdec839b
simonh-wincomplm <simonh@wincom-plm.com>
2023-01-15 00:24:12 +0100

    Add a check  for cyclomatic complexity
    Can be removed with

---
+++ wex-example-badapi/src/main/java/com/wincomplm/wex/example/badapi/examples/Cyclomatic.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2022 Wincom Consulting S.L.
+ * All Rights Reserved.
+ * This source is subject to the terms of a software license agreement.
+ * You shall not disclose such confidential information and shall use it only in accordance with the terms and conditions of the license agreement.
+ */
+
+package com.wincomplm.wex.example.badapi.examples;
+
+/**
+ *
+ * @author SimonHeath
+ */
+public class Cyclomatic {
+
+    void bad() {
+        if (true) {
+            if (true) {
+                if (true) {
+                    if (true) {
+                        if (true) {
+                            if (true) {
+                                if (true) {
+                                    if (true) {
+                                        if (true) {
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
