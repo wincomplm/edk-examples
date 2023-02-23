bdcfd0e1b3394a1f3607f7bcd4669d43e17ea0c2
nbouayada <53012088+nbouayada@users.noreply.github.com>
2023-02-23 16:26:19 +0100

    Added JUnit annotations for report generation
    	modified:   wex-example-security/pom.xml
    	modified:   wex-example-security/src/main/java/com/wincomplm/wex/example/security/junit/MainUiTest.java

--- wex-example-security/src/main/java/com/wincomplm/wex/example/security/junit/MainUiTest.java
+++ wex-example-security/src/main/java/com/wincomplm/wex/example/security/junit/MainUiTest.java
@@ -1,12 +1,13 @@
 package com.wincomplm.wex.example.security.junit;
 
+import com.wincomplm.wex.security.doc.annotations.TestDef.Category;
 import com.wincomplm.wex.junit.impl.test.JunitTestAbstract;
 import org.junit.Test;
 import org.junit.Before;
 import org.junit.After;
+import com.wincomplm.wex.security.doc.annotations.TestDef;
 
 public class MainUiTest extends JunitTestAbstract {
-
 
     @Before
     public void setUp() {
@@ -19,12 +20,34 @@ public class MainUiTest extends JunitTestAbstract {
     }
 
     @Test
+    @TestDef(
+            title = "Functional test 1",
+            description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            category = Category.FUNCTIONAL
+    )
     public void functional() throws Exception {
-        driver.get(getAuthUrl() + "/netmarkets/jsp/com/wincomplm/wex/example/security/call.jsp"); 
+        driver.get(getAuthUrl() + "/netmarkets/jsp/com/wincomplm/wex/example/security/call.jsp");
     }
-    
+
     @Test
+    @TestDef(
+            title = "Security test 1",
+            description = "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?",
+            category = Category.SECURITY
+    )
+
     public void security() throws Exception {
+    }
+
+    @Test
+    @TestDef(
+            title = "Other test 1",
+            description = "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.",
+            category = Category.OTHER,
+            otherCategory = "Custom"
+    )
+
+    public void other() throws Exception {
     }
 
 }
