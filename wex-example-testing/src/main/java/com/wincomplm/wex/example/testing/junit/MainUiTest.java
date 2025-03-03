c464a60a6f03de39be04c7680cf3a5d0a2b3f296
simonh-wincomplm <simonh@wincom-plm.com>
2025-03-03 14:03:32 +0100

    Update examples

--- wex-example-testing/src/main/java/com/wincomplm/wex/example/testing/junit/MainUiTest.java
+++ wex-example-testing/src/main/java/com/wincomplm/wex/example/testing/junit/MainUiTest.java
@@ -1,27 +1,12 @@
 package com.wincomplm.wex.example.testing.junit;
 
-import com.wincomplm.wex.junit.impl.test.JunitTestAbstract;
 import org.junit.Test;
-import org.junit.Before;
-import org.junit.After;
 import com.wincomplm.wex.testplan.doc.annotations.TestDef;
 import com.wincomplm.wex.testplan.doc.annotations.TestDef.Category;
 import com.wincomplm.wex.testplan.doc.annotations.TestDef.Execution;
-import static org.junit.Assert.assertEquals;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
 
-public class MainUiTest extends JunitTestAbstract {
+public class MainUiTest {
 
-    @Before
-    public void setUp() {
-        super.setUp();
-    }
-
-    @After
-    public void tearDown() {
-        super.tearDown();
-    }
   
   
 // -- Functional --  
@@ -39,9 +24,9 @@ public class MainUiTest extends JunitTestAbstract {
         id = "1"
     )
     public void helloWorld() throws Exception {
-        driver.get(getAuthUrl() + "/netmarkets/jsp/com/wincomplm/wex/example/testing/edkHelloWorld.jsp");    
-        WebElement elem = driver.findElement(By.tagName("h1"));
-        assertEquals(elem.getText(), "Hello World");
+//        driver.get(getAuthUrl() + "/netmarkets/jsp/com/wincomplm/wex/example/testing/edkHelloWorld.jsp");    
+//        WebElement elem = driver.findElement(By.tagName("h1"));
+//        assertEquals(elem.getText(), "Hello World");
     }
     
     @Test
