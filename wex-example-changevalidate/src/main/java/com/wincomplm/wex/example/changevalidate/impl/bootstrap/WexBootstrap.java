c043564660b461a7e7a14048c0f1387d65c7ee32
cromero-wincomplm <cromero@wincom-plm.com>
2025-12-15 15:16:11 +0100

    New Change Validator Example

---
+++ wex-example-changevalidate/src/main/java/com/wincomplm/wex/example/changevalidate/impl/bootstrap/WexBootstrap.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.wincomplm.wex.example.changevalidate.impl.bootstrap;
+
+import com.wincomplm.wex.kernel.impl.bootstrap.IWexBootstrap;
+import com.wincomplm.wex.kernel.impl.manager.IWexKernelPackage;
+import com.wincomplm.wex.system.impl.scan.WexScanner;
+
+/**
+ *
+ * @author Nassim Bouayad-Agha
+ */
+public class WexBootstrap implements IWexBootstrap {
+
+    @Override
+    public void initialize(IWexKernelPackage wex) throws Exception {
+        WexScanner.scan(wex, "com.wincomplm.wex.example.changevalidate");
+    }
+
+    @Override
+    public void terminate(IWexKernelPackage wex) throws Exception {
+    }
+
+}
